bb37972b81b3f180a4d0dbea247bc1bedbaa8f61
Minyi Zhong <minyi_z@hotmail.co.uk>
2018-02-14 00:56:24 +1100

    add substitution cipher init method which extends base

--- substitution_cipher.py
+++ substitution_cipher.py
@@ -3,30 +3,32 @@ from base_cipher import Cipher
 class SubstitutionCipher(Cipher):
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+	def __init__(self, key):
+		super().__init__(key)
+		self.substitution = self.get_substitution()
+
 	def check_key(self):
 		if not isinstance(self.key, str) or not self.key.isalpha() or len(self.key) > 26:
 			raise TypeError("Key must consist of letters and be less than or equal to 26 characters.")
 
 	def encipher(self, msg):
-		substitution = self.get_substitution()
 		enciphered_msg = ""
 
 		for char in msg:
 			if char.isalpha():
 				index = self.alphabet.index(char)
-				enciphered_msg += substitution[index]
+				enciphered_msg += self.substitution[index]
 			else:
 				enciphered_msg += char
 
 		return enciphered_msg
 
 	def decipher(self, msg):
-		substitution = self.get_substitution()
 		deciphered_msg = ""
 
 		for char in msg:
 			if char.isalpha():
-				index = substitution.index(char)
+				index = self.substitution.index(char)
 				deciphered_msg += self.alphabet[index]
 			else:
 				deciphered_msg += char
