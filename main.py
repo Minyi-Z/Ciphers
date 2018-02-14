32c20deb79f194c53df1d4bc1262b4ef6a01d1e7
Minyi Zhong <minyi_z@hotmail.co.uk>
2018-02-15 10:54:13 +1100

    update gitignore

--- main.py
+++
@@ -1,6 +0,0 @@
-from caesar_cipher import CaesarCipher
-from substitution_cipher import SubstitutionCipher
-
-cipher = SubstitutionCipher("b")
-print(cipher.encipher("abc"))
-print(cipher.decipher("bac"))
